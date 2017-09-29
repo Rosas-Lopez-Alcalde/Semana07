d646d479265f8e31f87d727cf5567983ea78fd4e
USP <USP@DESKTOP-TNO3FK2>
2017-09-29 15:33:46 -0500

    segundo AVANCE PROYECTO GRUPAL

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+class Tarea{
+    function mayorDosNumeros($n1,$n2){
+        if ($n1>$n2)
+            return $n1;
+        else
+            return $n2;
+    }
+    
+    
+    function MENORDosNumeros($n1,$n2){
+        
+            return 0;
+    
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
